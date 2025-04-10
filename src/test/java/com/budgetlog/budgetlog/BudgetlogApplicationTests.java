8ee8adf420abe9b5888d462f7d4c9ca147dd6f62
PARKyc <erty1201@naver.com>
2025-04-10 23:57:05 +0900

    add : 새로운 시작

---
+++ src/test/java/com/budgetlog/budgetlog/BudgetlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetlog.budgetlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
