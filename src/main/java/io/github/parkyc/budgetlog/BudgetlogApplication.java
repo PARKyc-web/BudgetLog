09bca9aaf25b75408cd1e8079a170b193f73c7d4
PARKyc <erty1201@naver.com>
2025-04-15 00:33:49 +0900

    Initial commit: Project setup with Spring Boot

---
+++ src/main/java/io/github/parkyc/budgetlog/BudgetlogApplication.java
@@ -0,0 +1,13 @@
+package io.github.parkyc.budgetlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/parkyc/budgetlog/BudgetlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.parkyc.budgetlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
