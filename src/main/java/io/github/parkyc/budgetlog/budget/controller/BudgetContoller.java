d614325c5810aae12b264ce89400d36611c1806e
PARKyc <erty1201@naver.com>
2025-05-22 23:08:49 +0900

    FrontEnd 통신 작업

--- src/main/java/io/github/parkyc/budgetlog/budget/controller/BudgetContoller.java
+++ src/main/java/io/github/parkyc/budgetlog/budget/controller/BudgetContoller.java
@@ -1,14 +1,14 @@
 package io.github.parkyc.budgetlog.budget.controller;
 
 import io.github.parkyc.budgetlog.budget.dto.BudgetDTO;
-import io.github.parkyc.budgetlog.budget.entity.Budget;
-import io.github.parkyc.budgetlog.common.response.CommonRes;
+import io.github.parkyc.budgetlog.budget.service.BudgetService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.parkyc.budgetlog.budget.service.BudgetService;
-import lombok.RequiredArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -23,4 +23,17 @@ public class BudgetContoller {
 
     return new BudgetDTO().builder().budgetName("test").build();
   }
+
+  @GetMapping("/list")
+  public List<BudgetDTO> getMyBudgetList(){
+
+    List<BudgetDTO> budgetList = new ArrayList<>();
+    for(int i = 0; i < 10; i++){
+      BudgetDTO dto = BudgetDTO.builder().budgetSeq(Long.valueOf(i)).budgetName("Sample Budget "+i).build();
+
+      budgetList.add(dto);
+    }
+    return budgetList;
+  }
+
 }
--- src/main/java/io/github/parkyc/budgetlog/common/response/CommonRes.java
+++ src/main/java/io/github/parkyc/budgetlog/common/response/CommonRes.java
@@ -12,30 +12,30 @@ import lombok.Setter;
 @Builder
 public class CommonRes<T> {
 
-  private final T data;
-  private final boolean success;
-  private final String code;
-  private final String message;
+    private final T data;
+    private final boolean success;
+    private final String code;
+    private final String message;
 
-  public CommonRes(){
-    this.data = null;
-    this.success = false;
-    this.code = "500 Internal Server Error";
-    this.message = "500 Internal Server Error";
-  }
+    public CommonRes(){
+        this.data = null;
+        this.success = false;
+        this.code = "500 Internal Server Error";
+        this.message = "500 Internal Server Error";
+    }
 
-  public CommonRes(T data){
-    this.data = data;
-    this.success = true;
-    this.code = "200 OK";
-    this.message = "200 OK";
-  }
+    public CommonRes(T data){
+        this.data = data;
+        this.success = true;
+        this.code = "200 OK";
+        this.message = "200 OK";
+    }
 
-  public CommonRes(String code, String message){
-    this.data = null;
-    this.success = false;
-    this.code = code;
-    this.message = message;
-  }
+    public CommonRes(String code, String message){
+        this.data = null;
+        this.success = false;
+        this.code = code;
+        this.message = message;
+    }
 
 }
